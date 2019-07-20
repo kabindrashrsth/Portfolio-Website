ac8cf577464c7f3bdf7cc1077d96d92881643499
Kabindra Shrestha <kabindra.shrestha@vanderbilt.edu>
2019-07-20 18:11:50 -0500

    Commit before deleting about, and project pages

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
+//const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
 //Set Initial State Of Menu
@@ -17,7 +17,7 @@ function toggleMenu() {
     menuBtn.classList.add("close");
     menu.classList.add("show");
     menuNav.classList.add("show");
-    menuBranding.classList.add("show");
+    //menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
     //Set Menu State
@@ -26,10 +26,57 @@ function toggleMenu() {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
-    menuBranding.classList.remove("show");
+    //menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
     //Set Menu State
     showMenu = false;
   }
+}
+
+// Scroll reveal
+
+window.sr = ScrollReveal();
+sr.reveal(".lg-heading", {
+  duration: 1800,
+  origin: "top",
+  distance: "30px"
+});
+
+sr.reveal(".ktm-img", {
+  duration: 1800,
+  origin: "bottom",
+  distance: "100px"
+});
+
+sr.reveal(".sm-heading", {
+  duration: 1800,
+  origin: "top"
+});
+
+// Scrolling and highlighing
+
+let navItem = document.querySelectorAll(".nav-item");
+// let about = document.getElementById("about");
+
+// about.addEventListener("mouseover", h);
+
+// function h() {
+//   console.log(7);
+//   for (let i = 0; i < 5; i++) {
+//     navItem[i].classList.remove("current");
+//   }
+//   navItem[1].classList.add("current");
+// }
+
+for (let i = 0; i < 5; i++) {
+  navItem[i].addEventListener("click", highlight);
+}
+
+function highlight(e) {
+  for (let i = 0; i < 5; i++) {
+    navItem[i].classList.remove("current");
+  }
+
+  e.target.parentNode.classList.add("current");
 }
